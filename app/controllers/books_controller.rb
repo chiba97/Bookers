45e2570188ca4cb71d6aa87380846a2cfd4b2522
chiba97 <ktfkyrya@gmail.com>
2021-08-21 15:38:26 +0000

    [Add]サクセスメッセージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,40 @@ class BooksController < ApplicationController
     book.save
     redirect_to book_path(book.id)
   end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice:"Book was successfully created"
+    else render:index
+    end
+  end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+  def update
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice:"Book was successfully updated"
+    else render:index
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
